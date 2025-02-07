1305d5d530f8ae9bf757ce0a31a78becce44b339
Santiago Gonzalez <punigonzalez07@gmail.com>
2025-02-07 18:02:51 -0300

    first commit

---
+++ src/main/java/com/elmanalubricentro/ElMana/ElManaApplication.java
@@ -0,0 +1,13 @@
+package com.elmanalubricentro.ElMana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElManaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElManaApplication.class, args);
+	}
+
+}
