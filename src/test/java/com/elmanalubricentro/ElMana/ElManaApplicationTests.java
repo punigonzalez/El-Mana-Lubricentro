c998586fef0d3144211f982f71b3902f63db3e35
Santiago Gonzalez <punigonzalez07@gmail.com>
2025-02-07 18:02:51 -0300

    first commit

---
+++ src/test/java/com/elmanalubricentro/ElMana/ElManaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elmanalubricentro.ElMana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElManaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
