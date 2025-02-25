57b136915eeb12262f580302dd8b8ca144883978
Santiago Gonzalez <punigonzalez07@gmail.com>
2025-02-25 17:22:28 -0300

    Se modifica ventas controller y repository

--- src/main/java/com/elmanalubricentro/ElMana/venta/controller/VentaController.java
+++ src/main/java/com/elmanalubricentro/ElMana/venta/controller/VentaController.java
@@ -1,10 +1,23 @@
 package com.elmanalubricentro.ElMana.venta.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.elmanalubricentro.ElMana.venta.entity.VentaRequestDTO;
+import com.elmanalubricentro.ElMana.venta.entity.Venta;
+import com.elmanalubricentro.ElMana.venta.service.VentaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/ventas")
-
+@RequestMapping("/api/ventas")
 public class VentaController {
+
+    @Autowired
+    private VentaService ventaService;
+
+    @PostMapping
+    public ResponseEntity<Venta> crearVenta(@RequestBody VentaRequestDTO ventaDTO) {
+        Venta nuevaVenta = ventaService.crearVenta(ventaDTO);
+        return new ResponseEntity<>(nuevaVenta, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/elmanalubricentro/ElMana/venta/repository/VentaRepository.java
+++ src/main/java/com/elmanalubricentro/ElMana/venta/repository/VentaRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VentaRepository extends JpaRepository<Venta,Long> {
-
+//hla
 
 }
